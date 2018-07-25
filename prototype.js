787fd04af41253c4769fd2ba7fa29e5ec5c80a56
charankrishna <charan.nithinj@gmail.com>
2018-07-25 11:26:10 +0530

    prototypes.js

---
+++ prototype.js
@@ -0,0 +1,11 @@
+function Student(name,age,gender,standard){
+Student.prototype.name=name;
+Student.prototype.age=age;
+Student.prototype.gender=gender;
+Student.prototype.standard=standard;
+}
+
+
+
+var student1 = new Student(carl,12,male,VI);
+
