1d5eb03ce43c25078d8898a65fd1ae00434649e5
charankrishna <charan.nithinj@gmail.com>
2018-07-26 07:08:03 +0530

    class

--- prototype.js
+++ prototype.js
@@ -10,9 +10,11 @@ Student.call(this,name,age,gender);
 this.subject=subject;
 }
 
-Student.prototype= new Teacher();
+Teacher.prototype = Object.create(Student.prototype);
+Teacher.prototype.constructor = Object.create(Person.prototype);
+
 
 var teacher1 = new Teacher("mark",27,"male","Mathematics");
-
+console.log(teacher1);
 
 
